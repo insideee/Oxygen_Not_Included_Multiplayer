dfe6b9ed25eac74cd9540385835829125ebec6e0
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2026-04-08 18:25:47 +0100

    Test duplicant unit test

---
+++ ClassLibrary1/DebugTools/UnitTests/DuplicantTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ONI_MP.DebugTools.UnitTests
+{
+    public static class DuplicantTests
+    {
+        [UnitTest(category: "Duplicant")]
+        public static UnitTestResult HasDuplicantSelected()
+        {
+            var selected = SelectTool.Instance?.selected;
+            if (selected == null)
+                return UnitTestResult.Fail("No object selected");
+            var hasDuplicant = selected.TryGetComponent(out MinionIdentity _);
+            if (!hasDuplicant)
+                return UnitTestResult.Fail("Selected object is not a duplicant");
+            return UnitTestResult.Pass($"Selected object is the duplicant: {selected.name}");
+        }
+    }
+}
